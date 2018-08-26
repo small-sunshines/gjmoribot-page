167fe94f38cf481ec0afc58eb7abe53e580fc34c
small_sunshine <jioo0224@naver.com>
2018-08-26 13:16:07 +0900

    소개, 구동환경 수정

--- pages/running.vue
+++ pages/running.vue
@@ -2,6 +2,33 @@
 div
   p.menu-label.is-hidden-touch running
   hr
+  .content
+    h1 모리봇 구동환경
+    table
+      thead
+        tr
+          td 부품
+          td 부품이름
+      tbody
+        tr
+          td CPU
+          td Intel i3-2100
+        tr
+          td RAM
+          td DDR3-10600 4GB 1ea, DDR3-12800 4GB 1ea
+        tr
+          td SSD
+          td TAMMUZ RX631 64GB
+        tr
+          td HDD
+          td MARSHAL MAL21000SA-T54 2.5in 1TB
+        tr
+          td PSU
+          td MICRONICS CYCLONE II
+        tr
+          td Docker
+          td Yes
+
 </template>
 
 <script>
