a58678471f6904879ab2adc9f99547cc8975bf8d
small_sunshine <jioo0224@naver.com>
2018-09-08 15:45:01 +0900

    sidebar 번역 완료

--- locales/en.js
+++ locales/en.js
@@ -14,6 +14,20 @@ export default {
   footer: {
     copyright: 'Copyright 2018. 볕뉘 all rights reserved.'
   },
+  sidebar: {
+    home: 'Home',
+    introduce: 'Introduce',
+    bot: 'MoriBot',
+    functions: 'Functions',
+    command: 'Command',
+    chatCommand: 'Chat Command',
+    admin: 'Admin Function',
+    inline: 'Inline Query',
+    env: 'Environment',
+    server: 'Server',
+    discord: 'Discord',
+    kaorukoBot: 'KaorukoBot'
+  },
   index: {
     hello: 'Hello. I am Mori.',
     telegram: 'MoriBot with Telegram',
